23070d57fe26bbdf015146988fe05f098ea3241a
sahil <shlrjsrivastava@gmail.com>
2024-08-26 22:26:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./Component/Header";
+import Body from "./Component/Body";
+import Footer from "./Component/Footer";
+
+export default function App() {
+    return (
+        <>
+
+            <Header />
+            <Body />
+            <Footer/>
+        </>
+
+    );
+}
